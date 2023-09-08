451c66f7bb8bad909c91fd36cabcae87b91dc333
James Jilhaney <notail2111@gmail.com>
2023-09-09 01:17:08 +0800

    Succesful integration of dadbod to nvim

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    lazy = true,
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.completion = {
+        completeopt = "menu,menuone,noinsert",
+      }
+      opts.window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      }
+      return opts
+    end,
+  },
+}
--- nvim/lua/plugins/dadbod.lua
+++ nvim/lua/plugins/dadbod.lua
@@ -1,3 +1,9 @@
 return {
-  "tpope/vim-dadbod",
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", lazy = true, ft = { "sql", "mysql", "plsql" } },
+    },
+  },
 }
