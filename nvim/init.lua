be71cb0b5241596c97129ff77667285bff5631fd
James Jilhaney <notail2111@gmail.com>
2024-03-22 23:44:26 +0800

    feat: added support for nvim-tmux-navigation

--- nvim/init.lua
+++ nvim/init.lua
@@ -185,10 +185,14 @@ vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+-- vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+-- vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+-- vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+-- vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<A-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+vim.keymap.set("n", "<A-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+vim.keymap.set("n", "<A-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+vim.keymap.set("n", "<A-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
