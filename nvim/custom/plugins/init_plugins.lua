8f0b4ee00179e2057285aa28198a495e2b177600
James Jilhaney <notail2111@gmail.com>
2024-03-28 13:02:20 +0800

    fix: buffer shortcuts

--- nvim/custom/plugins/init_plugins.lua
+++ nvim/custom/plugins/init_plugins.lua
@@ -28,6 +28,14 @@ local plugins = {
 			require("bufferline").setup({
 				options = {
 					separator_style = "slant",
+					always_show_bufferline = true,
+				},
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "File Explorer",
+						text_align = "center",
+					},
 				},
 			})
 		end,
@@ -36,8 +44,8 @@ local plugins = {
 			{ "<A-2>", "<cmd>BufferLineGoToBuffer 2<CR>", desc = "Go to buffer 2" },
 			{ "<A-3>", "<cmd>BufferLineGoToBuffer 3<CR>", desc = "Go to buffer 3" },
 			{ "<A-4>", "<cmd>BufferLineGoToBuffer 4<CR>", desc = "Go to buffer 4" },
-			{ "b[", "<cmd>BufferLineCycleNext<CR>", desc = "Go to next buffer" },
-			{ "b]", "<cmd>BufferLineCyclePrev<CR>", desc = "Go to previous buffer" },
+			{ "<C-S-L>", "<cmd>BufferLineCycleNext<CR>", desc = "Go to next buffer" },
+			{ "<C-S-H>", "<cmd>BufferLineCyclePrev<CR>", desc = "Go to previous buffer" },
 		},
 	},
 	{
