2014b30116ef3df53a806cae6daf016702e14dbe
James Jilhaney <notail2111@gmail.com>
2024-03-31 17:30:21 +0800

    chore: added copilot chat to deps

--- nvim/custom/plugins/init_plugins.lua
+++ nvim/custom/plugins/init_plugins.lua
@@ -668,7 +668,33 @@ local plugins = {
 			{ "<leader>ft", "<cmd>FloatermToggle<CR>", desc = "[F]loat [T]erm" },
 		},
 	},
-	{ "moll/vim-bbye" },
+	{ "moll/vim-bbye" }, -- Better closing of buffers
+	{
+
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		opts = {
+			debug = true, -- Enable debugging
+			-- See Configuration section for rest
+		},
+		lazy = false,
+		keys = {
+			{ "<leader>cc", "<cmd>CopilotChatToggle<CR>", desc = "[C]opilot [C]hat" },
+		},
+		-- config = function()
+		-- 	require("copilot").setup({
+		-- 		keys = {
+		-- 			{ "n", "<leader>cc", "<cmd>CopilotChatToggle<CR>", desc = "[C]opilot [C]hat" },
+		-- 		},
+		-- 	})
+		-- end,
+
+		-- See Commands section for default commands if you want to lazy load on them
+	},
 }
 
 return plugins
