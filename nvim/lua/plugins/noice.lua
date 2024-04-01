15c7a253385b2ae79defdc6c58a351d38da81461
James Jilhaney <notail2111@gmail.com>
2024-04-01 15:06:59 +0800

    chore: small changes
    chore: small changes

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -15,20 +15,41 @@ return {
 			-- you can enable a preset for easier configuration
 			presets = {
 				bottom_search = false, -- use a classic bottom cmdline for search
-				command_palette = false, -- position the cmdline and popupmenu together
+				command_palette = true, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
 				lsp_doc_border = false, -- add a border to hover docs and signature help
 			},
+			notify = {
+				enabled = false,
+			},
 			views = {
 				cmdline_popup = {
-					border = {
-						style = "none",
-						padding = { 2, 3 },
+					position = {
+						row = 5,
+						col = "50%",
 					},
-					filter_options = {},
+					size = {
+						width = 60,
+						height = "auto",
+					},
+				},
+				popupmenu = {
+					relative = "editor",
+					position = {
+						row = 8,
+						col = "50%",
+					},
+					size = {
+						width = 60,
+						height = 10,
+					},
+					border = {
+						style = "rounded",
+						padding = { 0, 1 },
+					},
 					win_options = {
-						winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+						winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
 					},
 				},
 			},
@@ -43,6 +64,6 @@ return {
 		-- OPTIONAL:
 		--   `nvim-notify` is only needed, if you want to use the notification view.
 		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
+		-- "rcarriga/nvim-notify",
 	},
 }
