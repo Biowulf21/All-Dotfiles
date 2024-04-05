b87ac7c230d05d89c57f6c16d158397f95c2fb0d
James Jilhaney <jamespatrickjilhaney@THINKBIsMini359.bbrouter>
2024-04-05 08:28:16 +0800

    chore: added codesnap as deps

---
+++ nvim/lua/plugins/codesnap.lua
@@ -0,0 +1,15 @@
+return {
+	"mistricky/codesnap.nvim",
+	build = "make",
+	config = function()
+		require("codesnap").setup({
+			has_breadcrumbs = true,
+			bg_theme = "bamboo",
+		})
+	end,
+
+	keys = {
+		{ "<leader>cc", "<cmd>CodeSnap<CR>", desc = "[C]opy [C]ode" },
+	},
+	lazy = false,
+}
