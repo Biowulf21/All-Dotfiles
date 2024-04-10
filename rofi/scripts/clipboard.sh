52838e8153e78a4c46e26e32161f8130dd187437
James Jilhaney <jamesjilhaney21@gmail.com>
2024-04-10 15:52:19 +0800

    chore: improved rofi config

--- rofi/scripts/clipboard.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get the selected item in rofi
-selected=$(~/clipster/clipster -o -n 50 | /usr/bin/rofi -dmenu)
-
-# Copy the selected item to the clipboard
-echo -n "$selected" | xclip -selection clipboard
