2a48ba26c9aea65d47c4492440cc7dff8213c7c5
James Jilhaney <jamesjilhaney21@gmail.com>
2024-07-20 08:06:32 +0800

    chore:update trouble nvim configuration

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -412,13 +412,65 @@ return {
 		"folke/trouble.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
+			icons = {
+				indent = {
+					middle = " ",
+					last = " ",
+					top = " ",
+					ws = "│  ",
+				},
+			},
+			modes = {
+				diagnostics = {
+					groups = {
+						{ "filename", format = "{file_icon} {basename:Title} {count}" },
+					},
+					-- cascade = {
+					-- 	mode = "diagnostics", -- inherit from diagnostics mode
+					-- 	filter = function(items)
+					-- 		local severity = vim.diagnostic.severity.HINT
+					-- 		for _, item in ipairs(items) do
+					-- 			severity = math.min(severity, item.severity)
+					-- 		end
+					-- 		return vim.tbl_filter(function(item)
+					-- 			return item.severity == severity
+					-- 		end, items)
+					-- 	end,
+					-- },
+				},
+			},
 		},
 		keys = {
-
-			{ "<leader>tt", "<cmd>TroubleToggle<CR>", desc = "[T]oggle [T]rouble" },
+			{
+				"<leader>tt",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>tT",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
 		},
 	},
 	-- "gc" to comment visual regions/lines
