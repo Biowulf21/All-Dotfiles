4e2f54928c848c90bdae9df78f816c7b9a53d838
James Jilhaney <notail2111@gmail.com>
2024-08-18 21:52:55 +0800

    chore: removed nvim backup file

--- nvim_bak/init.lua
+++
@@ -1,4 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-vim.opt.termguicolors = true
-
-require("config.lazy")
--- nvim_bak/lua/plugins/dadbod.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "kristijanhusak/vim-dadbod-ui",
-    dependencies = {
-      { "tpope/vim-dadbod", lazy = true },
-      { "kristijanhusak/vim-dadbod-completion", lazy = true, ft = { "sql", "mysql", "plsql" } },
-    },
-  },
-}
