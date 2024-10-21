cecf93ac8387160c0b39e5f8eb2de0a7fb6e22f9
James Jilhaney <jamespatrickjilhaney@THINKBIsMini359.bbrouter>
2024-10-21 15:04:33 +0800

    chore: updated theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -5,8 +5,9 @@ return {
 	priority = 1000,
 	config = function()
 		require("catppuccin").setup({
-			flavour = "mocha",
+			transparent_background = true,
 		})
-		vim.cmd.colorscheme("catppuccin")
+
+		vim.cmd.colorscheme("catppuccin-mocha")
 	end,
 }
