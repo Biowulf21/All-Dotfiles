50b992f1f41d3edce6453cabdb15eb6c55e6e23d
James Jilhaney <notail2111@gmail.com>
2024-10-23 22:52:27 +0800

    chore: configure mini.nvim modules

--- .config/nvim/lua/plugins/general.lua
+++ .config/nvim/lua/plugins/general.lua
@@ -1,24 +1,31 @@
 return {
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-	{ -- Collection of various small independent plugins/modules
-		"echasnovski/mini.nvim",
+	{
+		"echasnovski/mini.ai",
+		version = false,
 		config = function()
-			-- Better Around/Inside textobjects
-			--
-			-- Examples:
-			--  - va)  - [V]isually select [A]round [)]paren
-			--  - yinq - [Y]ank [I]nside [N]ext [']quote
-			--  - ci'  - [C]hange [I]nside [']quote
 			require("mini.ai").setup({ n_lines = 500 })
-
-			-- Add/delete/replace surroundings (brackets, quotes, etc.)
-			--
-			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-			-- - sd'   - [S]urround [D]elete [']quotes
-			-- - sr)'  - [S]urround [R]eplace [)] [']
-			-- require("mini.surround").setup()
 		end,
 	},
+
+	{
+		"echasnovski/mini.surround",
+		version = false,
+		opts = {
+			mappings = {
+				add = "gza", -- Add surrounding in Normal and Visual modes
+				delete = "gzd", -- Delete surrounding
+				find = "gzf", -- Find surrounding (to the right)
+				find_left = "gzF", -- Find surrounding (to the left)
+				highlight = "gzh", -- Highlight surrounding
+				replace = "gzr", -- Replace surrounding
+				update_n_lines = "gzn", -- Update `n_lines`
+			},
+		},
+	},
+	{ "echasnovski/mini.cursorword", version = false },
+	{ "echasnovski/mini.move", version = false },
+
 	{ -- Useful plugin to show you pending keybinds.
 		"folke/which-key.nvim",
 		event = "VimEnter", -- Sets the loading event to 'VimEnter'
